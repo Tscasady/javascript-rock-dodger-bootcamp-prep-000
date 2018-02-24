ac6dabc72446a5a325607ba940f7b4c49d8beb6c
Thomas Casady <tscasady@gmail.com>
2018-02-24 04:24:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval)
-  
+  totalRocks = document.getElementsByClassName
   for (let i = 0; i < ROCKS.length; i++) {
     rock.remove()
     
